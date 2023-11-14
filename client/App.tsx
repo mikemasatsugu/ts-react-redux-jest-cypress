0b200394f9179366c7601e3659acbc5ae9bba3b6
mikemasatsugu <mike.masatsugu@gmail.com>
2023-11-14 17:16:39 -0500

    react/express/webpack boilerplate done

---
+++ client/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return <h1>I love TypeScript</h1>
+};
+
+export default App;
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import path from 'path';
+import express, { Request, Response, NextFunction } from "express";
+import cookieParser from "cookie-parser";
+const PORT = 3000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('/', (req: Request, res: Response, next: NextFunction): void => {
+    try {
+        res.send('index.html');
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+});
